46bd41d03a8ddae2d3d39888c995387ab82adad2
Kavitha Megalingam <kavithamegalingam24@gmail.com>
2020-07-23 10:25:36 +0530

    Initial commit

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import './NavigationItems.css'
+import NavigationItem from './NavigationItem/NavigationItem'
+
+const navigationItems = (props) => (
+    <ul className = "NavigationItems">
+        <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+        <NavigationItem link="/orders">orders</NavigationItem>
+    </ul>
+)
+
+export default navigationItems;
